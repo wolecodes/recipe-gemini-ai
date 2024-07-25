449a796ce5cef71c3723ad33908a42b3663ba08f
oyeyemi toheeb <opeyemioyeyemi141@gmail.com>
2024-07-25 07:46:48 +0100

    fix some styles and restructure the prompt

--- prompt.js
+++ prompt.js
@@ -1,29 +1,30 @@
 const prompt = `
-I have uploaded an image. You are a chef. Identify the food in this image,.
+    You are a chef.
+    Identify the dish in this image, describe it, and suggest a recipe for this dish by specifying the ingredients and the instructions on how to make it.
+    explain step by step with detail
 
 If food is present, please provide a JSON object containing the following information:
 1. name (string): The common name of the dish.
-2. ingredients (list of strings): A list that describe it, and suggest a recipe for this dish by specifying the ingredients explain step by step with detail.
+2. ingredients (list of strings): A list that of the ingredients explain step by step with detail.
 3. Instruction (list of strings): A list containing high-level cooking instructions on how to make it.
 
 If food is not present, please provide a JSON object, notifying the error.
-
 Example:
 Input: (Image of a pizza)
 
 Output:
  {  
-    "name": "Pizza",
+    "name": "Sate Padang",
     "ingredients": [
-      "Dough",
-      "Tomato sauce",
-      "Cheese",
+      "1 pound of beef sirloin, cut into thin strips",
+      "1/2 cup of soy sauce",
+      "1/4 cup of vegetable oil",
     ],
     "instructions": [
-      "Prepare pizza dough",
-      "Spread tomato sauce",
-      "Add cheese and desired toppings",
-      "Bake until golden brown"
+      "In a large bowl, combine the beef, soy sauce, vegetable oil, garlic powder, coriander, cumin, turmeric, salt, and pepper. Stir to coat the beef evenly.",
+      " Cover the bowl and refrigerate for at least 30 minutes or overnight",
+      " Preheat the grill to medium-high heat",
+      "Thread the beef strips onto skewers"
     ],}
 
 Input: (image of a house)
